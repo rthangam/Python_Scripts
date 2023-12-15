f282d202a3b60dbeef4d46eb650f259a37d26218
Ramesh Thangamani <ramesh.thangamani@gmail.com>
2023-12-15 12:25:04 +0530

    Formatted file

--- python_server.py
+++ python_server.py
@@ -13,17 +13,17 @@ class HTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Origin', '*')
         http.server.SimpleHTTPRequestHandler.end_headers(self)
 
-def server(port):
-    httpd = socketserver.TCPServer(('', port), HTTPRequestHandler)
-    return httpd
+    def server(port):
+        httpd = socketserver.TCPServer(('', port), HTTPRequestHandler)
+        return httpd
 
-if __name__ == "__main__":
-    port = PORT
-    httpd = server(port)
-    try:
-        print("\nserving from build/ at localhost:" + str(port))
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        print("\n...shutting down http server")
-        httpd.shutdown()
-        sys.exit()
+    if __name__ == "__main__":
+        port = PORT
+        httpd = server(port)
+        try:
+            print("\nserving from build/ at localhost:" + str(port))
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            print("\n...shutting down http server")
+            httpd.shutdown()
+            sys.exit()
